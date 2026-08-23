cb156303e57eff94dd4bf585983f6912104167ed
Moataz Aldawood <moataz.aldawood@pm.me>
2026-08-23 23:07:44 +0300

    chore: remove build folders from repo and moved to C:\Users\moata\builds\Andalina

--- dist/builder/test.js
+++
@@ -1,7 +0,0 @@
-const { DOMParser, parseHTML } = require('linkedom');
-const mainDoc = parseHTML('<html></html>').document;
-const parser = new DOMParser();
-const includeDoc = parser.parseFromString('<head><link href="a.css"></head>', 'text/html');
-const walker = mainDoc.createTreeWalker(includeDoc, -1);
-const h = includeDoc.querySelector('head');
-console.log(Array.from(h.childNodes).map(n => n.nodeName));
--- dist/testing/src-code-example.js
+++
@@ -1,7 +0,0 @@
-function helloAndalina() 
-{
-    console.log("This is raw source code.");
-    const htmlSnippet = "<div class='test'>It escapes HTML correctly!</div>";
-    return htmlSnippet;
-}
-
